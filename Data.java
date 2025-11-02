7e71fedb89598edf583eaf326825e309377f22fd
sumanth3885UX <sumanth3885@gmail.com>
2025-11-02 11:29:49 -0500

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+
+
+
+class Data{
+    public static void main(String[] args) {
+       float chinni = 10.4444f;
+       int vishal = (int) chinni;
+       System.out.println(chinni);
+       System.out.println(vishal);
+       System.out.println(Integer.SIZE);
+
+       
+       Data east = new Data();
+       east.Sumanth();
+       east.Tinku();
+System.out.println("Enter salary");
+
+Scanner sc = new Scanner(System.in);
+int salary = sc.nextInt();
+System.out.println("Salary is: " + salary*2); 
+       
+       
+       }
+       
+       public void Sumanth(){
+        System.out.println("Hello Sumanth");
+
+
+
+       }
+
+public void Tinku(){
+
+
+    System.out.println("Hello Tinku");
+}
+
+
+    }
